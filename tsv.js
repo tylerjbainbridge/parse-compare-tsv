dec333a5614d7f56a2c008b486313c52c09882f1
Bainbridge, Tyler <ar-tyler.bainbridge@raq10-122.local>
2016-06-13 15:24:59 -0400

    Row equality test

--- tsv.js
+++ tsv.js
@@ -4,8 +4,18 @@ const csv = require('fast-csv');
 const async = require('async');
 const _ = require('lodash');
 
-var actualFile = 'actual.csv';
-var expectedFile = 'expected.csv';
+var args = process.argv.slice(2);
+var actualFile = args[0];
+var expectedFile = args[1];
+
+if (!actualFile) {
+  console.log('Must provide actual file');
+  process.exit();
+}
+if (!expectedFile) {
+  console.log('Must provide expected file');
+  process.exit();
+}
 
 async.parallel([
     (callback)=>{
@@ -38,7 +48,11 @@ async.parallel([
 });
 
 function compareFiles(actual, expected){
-  if(_.size(actual) == _.size(expected)){
-    console.log(`Both files contain ${actual.length} rows.`)
+  if(_.size(actual) === _.size(expected)){
+    for(let i=0; i<actual.length; i++){
+      if(!_.isEqual(actual[i], expected[i])){
+        console.log(`Error on row ${i}`);
+      }
+    }
   }
 }
